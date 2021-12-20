d01f9d2cbe46f9ed0d719f45ade26bdd5b55d155
khanhtran01 <butbixanh20@gmail.com>
2021-12-20 14:43:44 +0700

    commit

--- main.py
+++ main.py
@@ -8,32 +8,49 @@ MIN_USER_VOTED = 3 # so voted 1 phim it nhat phai co
 MIN_MOVIE_VOTED = 50 # so phim it nhat 1 user phai vote
 
 np.set_printoptions(suppress=True)
+# doc file
 movies = pd.read_csv('movies.csv', low_memory=False)
 ratings = pd.read_csv('ratings.csv', low_memory=False)
+# asc = file rating sort theo userId va time
 asc = ratings.sort_values(by=['userId', 'timestamp'])
+# đếm số lượt được voted của film
 no_user_voted = ratings.groupby('movieId')['rating'].agg('count')
+# đếm số lần vote của 1 user
 no_movies_voted = ratings.groupby('userId')['rating'].agg('count')
-# print(no_user_voted[no_user_voted>3].index)
-# print(ratings)
-# print(no_movies_voted[no_movies_voted>MIN_USER_VOTED])
-# new_rating = ratings.loc[ratings['userId'] == no_user_voted[no_user_voted>3].index]
+# lọc ra những user và movie không thỏa điều kiện
 ratings = ratings[ratings.movieId.isin(no_user_voted[no_user_voted> MIN_MOVIE_VOTED].index)]
 ratings = ratings[ratings.userId.isin(no_movies_voted[no_movies_voted>MIN_USER_VOTED].index)]
-def getListUid(uid):
-    ratings_temp = ratings    
-    final_dataset = ratings_temp.pivot(index='movieId',columns='userId',values='rating')
-    final_dataset = final_dataset.fillna(final_dataset.mean(axis=0))
-    desc = ratings_temp[ratings_temp['userId'] == uid].sort_values(by='timestamp', ascending=False)
-    arr = desc.head(5).values
-    movie_list = arr[:,1]
-    final_dataset = final_dataset.loc[movie_list]
-    final_dataset.fillna(0,inplace=True)
 
+def getListUid(uid):
+    """Trả về danh sách userId giống với uid"""
+    ratings_temp = ratings    
+    # chuyển dữ liệu thành bảng 2x2 với row = movideId, column = userId và value = rating
+    final_dataset = ratings_temp.pivot(index='movieId',columns='userId',values='rating')
+    # set các giá trị N/A thành giá trị trung bình của column tương ứng
+    # final_dataset = final_dataset.fillna(final_dataset.mean(axis=0))
+
+    #sort file rating theo userId và time
+    desc = ratings_temp[ratings_temp['userId'] == uid].sort_values(by='timestamp', ascending=False)
+    # lấy 5 giá trị có time lớn nhất cho user đã chọn
+    arr = desc.head(5).values
+    # chuyển sang dạng list và tách chỉ lấy list movie
+    movie_list = arr[:,1]
+    
+    
+    # trên dữ liệu final_dataset chỉ lấy những bộ film có trong list ở trên
+    final_dataset = final_dataset.loc[movie_list]
+    #set các giá trị N/A thành 0
+    final_dataset.fillna(0,inplace=True)
+    # xoay bảng để row = userId và clolumn = movieId
+    # phải xoay theo định dạng của hàm knn sẽ sử dụng
     final_dataset = final_dataset.T
     new_arr=  np.array(final_dataset)
     list_data = new_arr.tolist()
+    # khai báo KNN và set giá giá trị thuật toán cho nó
     knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20)
+    # chọn dataset cho thuật toán
     knn.fit(list_data)
+    # trả về 10 user gần giống với uid nhất
     distances , indices =knn.kneighbors([list_data[0]],n_neighbors=10)
     return sorted(zip(distances.tolist()[0], indices.tolist()[0]))
 
@@ -45,6 +62,8 @@ def checksize (uid):
     return False
 
 def extend_mode(uid):
+    """sử dụng trong trường hợp uid là người dùng bị lọc
+    ở mode này thì dùng knn dựa trên số sao của bộ phim"""
     final_dataset = ratings.pivot(index='movieId',columns='userId',values='rating')
     final_dataset.fillna(0,inplace=True)
     no_user_voted = ratings.groupby('movieId')['rating'].agg('count')
@@ -79,18 +98,22 @@ def extend_mode(uid):
     return df
 
 def get_recommendation(uid):
+    # kiểm tra uid có thuộc diện bị lọc bnỏ hay k
     if (checksize(uid) == False):
         return extend_mode(uid)
     list_uid = getListUid(uid)
-    # print(list_uid)
+
     list_uid.pop(0)
     ratings_temp = ratings
-    test_id = ratings_temp[ratings_temp['userId'] == uid]
+    # test_id = ratings_temp[ratings_temp['userId'] == uid]
 
-    x = int(test_id.size/4)
+    # x = int(test_id.size/4)
     count_phim = []
     np.set_printoptions(suppress=True,\
     formatter={'float_kind':'{:0.0f}'.format})
+
+
+    # lọc qua từng user và đếm số vote >4 của từng bộ phim
     for i in list_uid:
         test_id = ratings_temp[ratings_temp['userId'] == i[1]]
         x = int(test_id.size/4)
@@ -99,8 +122,10 @@ def get_recommendation(uid):
                 count_phim.append(test_id.iloc[j].at['movieId'])
     unique, counts = np.unique(np.array(count_phim), return_counts=True)
     final_arr = np.asarray((unique, counts)).T.tolist()
+    # sort các bộ phim theo số lần được vote >= 4 nhiều nhấ
     top_recommendation = sorted(final_arr,key=lambda l:l[1], reverse=True)
     list_phim = []
+    # lấy được 10 bộ phim
     for i in range(10):
         list_phim.append(top_recommendation[i][0])
     list_movie_name = []
@@ -110,7 +135,6 @@ def get_recommendation(uid):
     df = pd.DataFrame(list_movie_name,index=range(1,10+1))
     return df
 
-# print(get_recommendation(2))
 if __name__ == "__main__":
     while(True):
         uid = int(input("Nhap user ID: "))
